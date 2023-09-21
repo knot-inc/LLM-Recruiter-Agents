2db0c37c1b4d4545dfc0c1f1605ab626ef71c49f
Tomoaki Imai <tomoima525@gmail.com>
2023-09-21 22:54:25 +0900

    out put in json format

--- src/predict_industry.ts
+++ src/predict_industry.ts
@@ -3,7 +3,21 @@ dotenv.config();
 
 import { initializeAgentExecutorWithOptions } from "langchain/agents";
 import { OpenAI } from "langchain/llms/openai";
+import { StructuredOutputParser } from "langchain/output_parsers";
+import { PromptTemplate } from "langchain/prompts";
 import { SerpAPI } from "langchain/tools";
+
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+  name: "Company name",
+  category: "Category of business",
+});
+
+const formatInstructions = parser.getFormatInstructions();
+const prompt = new PromptTemplate({
+  template: "Format input.\n{format_instructions}\n{input}",
+  inputVariables: ["input"],
+  partialVariables: { format_instructions: formatInstructions },
+});
 
 const model = new OpenAI({ temperature: 0 });
 const tools = [
@@ -22,12 +36,19 @@ const main = async () => {
     verbose: false,
   });
 
-  let input = `What is the category of business of this company? Answer only the category. If a website link is provided, use the link to make prediction, for example, search with "What does https://google.com do?".\n Company name: ${companyName}`;
+  let input = `What is the category of business of this company? If a website link is provided, use the link to make prediction, for example, search with "What does https://google.com do?".\n Company name: ${companyName}`;
   if (website) {
     input += `\n Website: ${website}`;
   }
   const result = await executor.call({ input });
-  console.log(result);
+
+  // Format answer
+  const format = await prompt.format({
+    input: result.output,
+  });
+  const response = await model.call(format);
+
+  console.log(await parser.parse(response));
 };
 
 main().catch((e) => {
